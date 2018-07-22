e8acdef73cf6b652ee46d9ec5d27fb77acde0737
Jonah Middleton <jonah.middleton@gmail.com>
2018-07-22 12:26:30 -0400

    Tweak code for submission
    Add comments, Make html more consistant, Move nav to cover all nav elements, Make small style tweaks

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ const staticCache = 'restaurant-reviews-static-';
 const staticVer = 'v5';
 
 self.addEventListener('install', event =>{
-
 	event.waitUntil(
+
+		// Precache main application files
 		caches.open(`${staticCache}${staticVer}`).then(cache => {
 			return cache.addAll(['/', '/restaurant.html',
 				'js/main.js', 'js/dbhelper.js', 'js/swhelper.js', 'js/restaurant_info.js',
@@ -12,11 +13,14 @@ self.addEventListener('install', event =>{
 				'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
 				'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js']);
 		})
+
 	);
 });
 
 self.addEventListener('activate', event =>{
 	event.waitUntil(
+
+		// Delete old caches
 		caches.keys().then(cacheNames =>{
 			return Promise.all(
 				cacheNames.filter(cacheName =>{
@@ -25,32 +29,40 @@ self.addEventListener('activate', event =>{
 				}).map(cacheName =>{
 					console.log(`Deleting ${cacheName}`)
 					return caches.delete(cacheName);
-				}) 
-			)
+				})
+			);
 		})
+
 	);
 });
 
 self.addEventListener('message', event => {
+
 	if(event.data.action === 'skipWaiting'){
 		self.skipWaiting();
 	}
+
 });
 
 self.addEventListener('fetch', event => {
 	event.respondWith(
+
+		// Look for request in cache
 		caches.open(`${staticCache}${staticVer}`).then(cache =>{
 			return cache.match(event.request).then(response => {
 
+				// Fetch requests not in cache
 				if(!response){
 					console.log(`${event.request.url} not in cache, fetching...`);
 					const fetchRequest = event.request.clone();
 					return fetch(fetchRequest).then(fetchResponse =>{
 						
+						// Cache new responses
 						const fr = fetchResponse.clone();
 						cache.put(event.request, fr);
 						return fetchResponse;
 					});
+
 				}else{
 					console.log(`${event.request.url} in cache, serving...`);
 					return response;
