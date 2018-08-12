d00776f087f64a741bce4f0039bddd4091e53e5c
Jonah Middleton <jonah.middleton@gmail.com>
2018-08-12 13:04:36 -0400

    Minify css and javascript

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const connect = require('gulp-connect');
 const sass = require('gulp-sass');
 const images = require('gulp-responsive-images');
 const wait = require('gulp-wait');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
 const del = require('del');
 
 
@@ -10,40 +12,56 @@ gulp.task('styles', gulp.series(cleanStyles, compileStyles));
 
 gulp.task('images', gulp.series(cleanImages, resizeImages));
 
-gulp.task('clean', gulp.parallel(cleanImages, cleanStyles));
+gulp.task('js', gulp.series(cleanJS, minifyJS))
 
-gulp.task('watch', gulp.parallel(watchStyles, watchImages));
+gulp.task('clean', gulp.parallel(cleanImages, cleanStyles, cleanJS));
 
-gulp.task('default', gulp.parallel(serve, gulp.series(gulp.parallel('styles', 'images'), 'watch')));
+gulp.task('watch', gulp.parallel(watchStyles, watchImages, watchJS));
+
+gulp.task('default', gulp.parallel(serve, gulp.series(gulp.parallel('styles', 'images', 'js'), 'watch')));
 
 
 function cleanStyles() {return del(['css/*']);}
 
 function cleanImages() {return del(['images/*']);}
 
-function reload() {return connect.reload();}
+function cleanJS() {return del(['js/*']);}
 
 function watchStyles() {
 	return gulp.watch('src/sass/*')
-		.on('all', gulp.series('styles', reload));
+		.on('all', gulp.series('styles'));
 }
 
 function watchImages() {
 	return gulp.watch('src/images/*')
-		.on('all', gulp.series('images', reload));
+		.on('all', gulp.series('images'));
+}
+
+function watchJS() {
+	return gulp.watch('src/js/*')
+		.on('all', gulp.series('js'));
 }
 
 function serve() {
-	return connect.server({
-		livereload: true
-	})
+	return connect.server()
 }
 
 function compileStyles() {
 	return gulp.src('src/sass/*')
-		.pipe(wait(50))								//Wait for file to finish saving
-		.pipe(sass().on('error', sass.logError))
+		.pipe(wait(50))		//Wait for file to finish saving
+		.pipe(sourcemaps.init())
+		.pipe(sass({style: 'compressed'}).on('error', sass.logError))
+		.pipe(sourcemaps.write('../maps'))
 		.pipe(gulp.dest('css'));
+}
+
+function minifyJS() {
+	return gulp.src('src/js/*')
+		.pipe(wait(50))		//Wait for file to finish saving
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write('../maps'))
+		.pipe(gulp.dest('js'));
 }
 
 function resizeImages() {
